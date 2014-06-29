bf6afe1d40e5e4f7991ae8f54d7a0502da5971f3
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2014-06-29 10:33:38 +0200

    It will not check out the repos, so let them do it themselves

--- cookbooks/ceres-pallas/recipes/default.rb
+++ cookbooks/ceres-pallas/recipes/default.rb
@@ -2,24 +2,3 @@ package "git"
 package "curl"
 
 include_recipe "nodejs"
-
-git "/ceres-pallas" do
-  repository "git://github.com/ceres-pallas/reinforcement-learning-explanation.git"
-  action :sync
-end
-
-git "/ceres-pallas" do
-  repository "git://github.com/ceres-pallas/asteroids-server.git"
-  action :sync
-end
-
-git "/ceres-pallas" do
-  repository "git://github.com/ceres-pallas/inverted-pendulum.git"
-  action :sync
-end
-
-execute "npm install" do
-  cwd "/ceres-pallas/asteroids-server" 
-  command "npm install"
-  creates "/ceres-pallas/asteroids-server/node_modules"
-end
